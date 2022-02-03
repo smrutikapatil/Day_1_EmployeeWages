b4d31043c8cf5f87e120071993f95038de1d8ebb
smrutikapatil <smruti.patil17@gmail.com>
2022-02-03 22:43:03 +0530

    Added- Calculate employee daily wage

---
+++ src/com/employeewage/EmpWageBuilderUC2.java
@@ -0,0 +1,17 @@
+package com.employeewage;
+
+public class EmpWageBuilderUC1 {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
+        int daily_Wage = 0;
+        int wage_Pr_Hr = 20;
+        int working_Hour = 0;
+        double emp_check = Math.floor(Math.random() * 10) % 2;
+        if (emp_check == IS_FULL_TIME)
+            working_Hour = 8;
+        else
+          working_Hour = 0;
+        daily_Wage = working_Hour * wage_Pr_Hr;
+        System.out.println("Employee daily wage is " + daily_Wage);
+    }
+}
